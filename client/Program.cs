a6a34ed34ac7d3048385cd99ce4c9541ebeca376
Mateus Rodrigues <mateusborges090@gmail.com>
2024-09-13 22:19:22 -0300

    Add project files.

---
+++ client/Program.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Messages;
+using System.Threading.Tasks;
+
+namespace client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:50005");
+
+            await BiDirectionalStreamingCallAsync(channel);
+        }
+
+        private static async Task BiDirectionalStreamingCallAsync(GrpcChannel channel)
+        {
+            var client = new MessageServices.MessageServicesClient(channel);
+
+            //array com valor das trxs para simulacao
+            int[] transactionsArray =
+            {
+        100, 500, 900, 300, 1000, 2000, 1500, 350, 980, 5530, 3210, 1230
+    };
+
+            var request = client.GetMaxValueRealTime();
+
+            var serverListenerTask = Task.Run(async () =>
+            {
+                await foreach (var response in request.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(response.Resultado + ". Saldo atual: " + response.SaldoAtual);
+                }
+            });
+
+            foreach (var transaction in transactionsArray)
+            {
+                Console.WriteLine($"Client --> Server || Enviando transação: {transaction}");
+                await request.RequestStream.WriteAsync(new SingleNumberMessage()
+                {
+                    Value = transaction
+                });
+                await Task.Delay(1000);
+            }
+
+            await request.RequestStream.CompleteAsync();
+            await serverListenerTask;
+        }
+
+    }
+}
---
+++ server/services/MessageServicesImpl.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using Messages;
+using System.Threading.Tasks;
+
+namespace server.services
+{
+    public class MessageServicesImpl : MessageServices.MessageServicesBase
+    {
+        private decimal _saldo = 5000; //saldo inicial
+
+        public override async Task GetMaxValueRealTime(
+            IAsyncStreamReader<SingleNumberMessage> requestStream,
+            IServerStreamWriter<SingleResponseMessage> responseStream,
+            ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                decimal valorTransacao = message.Value;
+
+                
+                Console.WriteLine($"Server: Processando transação: {valorTransacao}");         
+
+                await Task.Delay(500);
+
+                string resultadoTransacao;
+
+                if (_saldo >= valorTransacao)
+                {
+                    _saldo -= valorTransacao;
+                    resultadoTransacao = "Aprovada";
+                }
+                else
+                {
+                    resultadoTransacao = "Negada: Saldo Insuficiente";
+                    Console.WriteLine($"Server: Error");
+                }
+
+                //envia a resposta para o client
+                await responseStream.WriteAsync(new SingleResponseMessage
+                {
+                    Resultado = resultadoTransacao,
+                    SaldoAtual = (double)_saldo
+                });
+
+                await Task.Delay(500);
+            }
+        }
+    }
+}
